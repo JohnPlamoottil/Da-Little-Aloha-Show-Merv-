58d1019059fefaeda2bcfcb77ea510ee3759d2c4
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-11-22 17:00:14 -0600

    create shop page

---
+++ web/src/pages/Shop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Shop() {
+  return (
+    <div className="page-content">
+      <h1>Shop</h1>
+      <p>Browse our collection of merchandise and products.</p>
+    </div>
+  );
+}
+
+export default Shop;
