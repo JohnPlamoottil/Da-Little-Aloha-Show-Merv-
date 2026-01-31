9e4ec1cce268b21f7d4a7ce0c050ccb4e8717432
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2026-01-30 18:52:10 -0600

    The dropdown question text is now 50px in FAQs.jsx.

--- web/src/pages/FAQs.jsx
+++ web/src/pages/FAQs.jsx
@@ -87,7 +87,7 @@ function FAQs() {
                 </span>
                 <span
                   className="text-xl font-semibold flex-grow text-center"
-                  style={{ color: "white" }}
+                  style={{ color: "white", fontSize: "50px" }}
                 >
                   {faq.question}
                 </span>
