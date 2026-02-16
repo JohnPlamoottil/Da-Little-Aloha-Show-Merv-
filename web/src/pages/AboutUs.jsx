362959aef06f940def2b0d140a43cddbbf3f6121
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2026-02-16 12:16:17 -0600

    Adjusted the paragraph block to fit inside the background image by reducing size and tightening layout.

--- web/src/pages/AboutUs.jsx
+++ web/src/pages/AboutUs.jsx
@@ -27,6 +27,7 @@ function AboutUs() {
             src={aboutBackground}
             alt="About page background"
             className="about-background w-full rounded-lg shadow-lg block"
+            style={{ height: "1400px", objectFit: "cover" }}
           />
           <img
             src={alohaPoster}
@@ -45,31 +46,36 @@ function AboutUs() {
           <p
             style={{
               position: "absolute",
-              top: "43%",
+              top: "52%",
               left: "50%",
               transform: "translate(-50%, -50%)",
-              width: "80%",
-              fontSize: "25px",
+              width: "85%",
+              maxWidth: "1100px",
+              fontSize: "28px",
+              lineHeight: "1.3",
+              padding: "0 1.5rem",
               textAlign: "center",
               zIndex: 9,
             }}
           >
-            We are Da Little Aloha Show! Originally from the islands but now we
-            reside on the Big Island of Gurnee! We do love spreading the "Aloha
-            Spirit" at special events, parades, community gatherings, schools
-            and much more! We are a 3 person show, 1 Male & 2 Female. Mana,
-            Melanie and Lori. Our traditional Polynesian show is typically
-            40-5omins. We include a showcase of Polynesian dances from Hawaii,
-            Fiji, New Zealand - Haka! Tahiti, and we even have the famous "FIRE
-            DANCE" of Samoa! The Siva Afi! We includes, all professional sound
-            equipment, mic, tiki-torches and encourage/teach crowd
-            participation. Our cost is open to budget! We hope that you choose
-            us at your next community event!! Our feedback from other
-            municipalities and clients speaks for itself! Let us bring the
-            summer Island-Vibes to your gathering, we bring excitement! Energy!
-            and the Heat from our very own Fire-Dancer of Samoa! We can also
-            offer a custom Photo Booth experience & DeeJay Entertainment with an
-            Emcee to facilitate your next event! Mahalo Nui Loa!
+            We are Da Little Aloha Show! <br /> Originally from the islands but
+            now <br />
+            we reside on the Big Island of Gurnee! <br /> We do love spreading
+            the "Aloha Spirit" at special events, parades, community gatherings,
+            schools and much more! <br /> We are a 3 person show, 1 Male & 2
+            Female. <br /> Mana, Melanie and Lori. <br /> Our traditional
+            Polynesian show is typically 40-50mins. <br /> We include a showcase
+            of Polynesian dances from Hawaii, Fiji, New Zealand - Haka! Tahiti,
+            and we even have the famous "FIRE DANCE" of Samoa! The Siva Afi!{" "}
+            <br /> We includes, all professional sound equipment, mic,
+            tiki-torches and encourage/teach crowd participation. <br /> Our
+            cost is open to budget! <br /> We hope that you choose us at your
+            next community event!! <br /> Our feedback from other municipalities
+            and clients speaks for itself! <br /> Let us bring the summer
+            Island-Vibes to your gathering, we bring excitement! Energy! and the
+            Heat from our very own Fire-Dancer of Samoa! We can also offer a
+            custom Photo Booth experience & DeeJay Entertainment with an Emcee
+            to facilitate your next event! Mahalo Nui Loa!
           </p>
           <img
             src={mervActionPose}
@@ -77,7 +83,7 @@ function AboutUs() {
             className="about-action rounded-lg shadow-lg"
             style={{
               position: "absolute",
-              top: "58%",
+              top: "88%",
               left: "50%",
               transform: "translateX(-50%)",
               width: "35%",
