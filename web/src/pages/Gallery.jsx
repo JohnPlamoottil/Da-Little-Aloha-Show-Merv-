91530e20d90b662482fb96f390d72eb03d4028c1
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2026-02-16 15:07:21 -0600

    add 4 images into gallery

--- web/src/pages/Gallery.jsx
+++ web/src/pages/Gallery.jsx
@@ -2,6 +2,24 @@ import React from "react";
 
 const galleryBackgroundUrl =
   "https://res.cloudinary.com/ds1fxskme/image/upload/v1771267903/gallery_background_im9mwr.jpg";
+const galleryImages = [
+  {
+    src: "https://res.cloudinary.com/ds1fxskme/image/upload/v1769898856/merv_fire_knives_w4rul7.jpg",
+    alt: "Merv fire knives",
+  },
+  {
+    src: "https://res.cloudinary.com/ds1fxskme/image/upload/v1769898856/merv_stage_ziu5lu.jpg",
+    alt: "Merv on stage",
+  },
+  {
+    src: "https://res.cloudinary.com/ds1fxskme/image/upload/v1769898854/merv_1_dl1bk1.jpg",
+    alt: "Merv performance",
+  },
+  {
+    src: "https://res.cloudinary.com/ds1fxskme/image/upload/v1769898855/triodancers_os3faf.jpg",
+    alt: "Trio dancers",
+  },
+];
 
 function Gallery() {
   return (
@@ -17,6 +35,17 @@ function Gallery() {
       <div className="page-content">
         <h1 style={{ color: "white" }}>Gallery</h1>
         <p style={{ color: "white" }}>Browse our photo and video gallery.</p>
+        <div className="mt-8 grid grid-cols-2 gap-6">
+          {galleryImages.map((image) => (
+            <img
+              key={image.src}
+              src={image.src}
+              alt={image.alt}
+              className="w-full rounded-lg shadow-lg object-cover"
+              style={{ aspectRatio: "4 / 3" }}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
